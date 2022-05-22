7390735e1e8e698b46ff246083fdf6c656744fbe
Gemma Holz <Gemma.Holz@balticapprenticeships.com>
2022-05-22 13:01:08 +0100

    Add project files.

---
+++ CaseSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CaseSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numer");
+            int intNum = Convert.ToInt32(Console.ReadLine());   
+
+            switch (intNum)
+            {
+                case 19:
+                    Console.WriteLine("You are 19");
+                    break;
+                case 20:
+                    Console.WriteLine("You are 20");
+                    break;
+                default:
+                    Console.WriteLine("Your not 19 or 20");
+                    break;
+
+            }
+        }
+    }
+}
